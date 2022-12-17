88200981bdb32d7f1c20a39fd778762bc9e2714c
BorisKoba <browi@mail.ru>
2022-12-17 21:05:55 +0300

    HW-10 1/2

--- Peppers-Gallery/src/peppers.js
+++ Peppers-Gallery/src/peppers.js
@@ -1,11 +1,18 @@
 const detailsImage = document.querySelector(".details-image");
 const detailsTitle = document.querySelector(".details-title");
+const detailsContainer = document.querySelector(".details-container");
 const anchorElement = document.querySelectorAll(".thumbnails-anchor");
 const mainElement = document.querySelector(".main-class");
 const buttonElement = document.getElementById("hide-button");
+const IS_POINT ="is-point";
 const Hidden = "hidden";
 function showDetails(){
         mainElement.classList.remove(Hidden);
+        detailsContainer.classList.add(IS_POINT)
+        setTimeout(function(){
+                detailsContainer.classList.remove(IS_POINT);
+        });
+        
 }
 function hideDetails(){
         mainElement.classList.add(Hidden);
