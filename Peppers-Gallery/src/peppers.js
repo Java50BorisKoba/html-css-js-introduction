ec2e52c0bd851b3ca1f4ddd806107f708fa80186
BorisKoba <browi@mail.ru>
2022-12-27 19:56:19 +0300

    1

--- Peppers-Gallery/src/peppers.js
+++ Peppers-Gallery/src/peppers.js
@@ -4,9 +4,15 @@ const detailsContainer = document.querySelector(".details-container");
 const anchorElement = document.querySelectorAll(".thumbnails-anchor");
 const mainElement = document.querySelector(".main-class");
 const buttonElement = document.getElementById("hide-button");
+const audio = document.querySelector('audio');
 const IS_POINT ="is-point";
 const Hidden = "hidden";
 function showDetails(){
+        audio.src = anchor.getAttribute("data-details-bark");
+    audio.play();
+    setTimeout(function () {
+        audio.pause();
+    }, 3000)
         mainElement.classList.remove(Hidden);
         detailsContainer.classList.add(IS_POINT)
         setTimeout(function(){
