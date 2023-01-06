89907d231bcd59bdf81f97e6662331c8e4efcbff
BorisKoba <browi@mail.ru>
2023-01-06 09:10:00 +0300

    HW#14

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
-// const ar =[];
-// const ar1 = [1,2,3,4];
-// ar.push(...ar1);
-// ar.push('abc');
-function getRandomNumber(min,max){
-        min = Math.trunc(min);
-        max = Math.trunc(max);
-        return min + Math.trunc(Math.random() * (max-min + 1));
-}
-function getRandomMatrix(rows,columns,min,max){
-        const matrix = [];
-        for(let i = 0; i < rows;i++){
-                matrix.push([]);
-                for(let j =0 ;j < columns; j++){
-                        matrix[i].push(getRandomNumber(min,max));
-                }
-        }
-        return matrix;
-}
-const matrix = getRandomMatrix(3,4,0,1);
+const array = [10, 20, 30, -10, 11, 100];
+const index = array.indexOf(30);
+// removing
+// const res = array.splice(index, 1);
+//array.splice(index, undefined, -1000);
+//insert
+//array.splice(index,0,-10,-20);
+//replace
+// array.splice(index,2, 20);
+// const ar1 = [40,200];
+// array.splice(index, 1, ...ar1);
+// const numbersPerPage = 2;
+// const page = 1;
+// const firstIndex = (page - 1) * numbersPerPage;
+// const pageNumbers = array.slice(firstIndex, firstIndex + numbersPerPage);
+// const ar1 = array.map(multiply2);
+// function multiply2(number){
+//         return number * 2;
+// }  
+// const ar2 = array.map(function(number,index){
+//         return index + 1 +'. ' + number;
+// });
+const ar3 = array.map(function(number){
+        return `<li>${number}</li>`
+});
+const ulElement = `<ul>${ar3.join('')}</ul>`;
+
